c972766f2359fd05da255b72ab4520c27ce43b78
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2024-10-18 18:18:00 +0200

    add

--- js/main.js
+++ js/main.js
@@ -5,50 +5,67 @@ const number = document.querySelector('.number');
 const uppercase = document.querySelector('.uppercase');
 const charactersSpecial = document.querySelector('.characters-special');
 const bar = document.querySelector('.bar');
+const messagePasswordDom = document.querySelector('#messagePassword');
 let countValid = 0;
 
 
+function messagePassword(){
+    if(countValid === 1){
+        messagePasswordDom.innerHTML=`<h4 class="mt-5 text-danger" id="messagePassword">Password Debole</h4>`
+    } else if(countValid === 2){
+        messagePasswordDom.innerHTML=`<h4 class="mt-5" style="color:#ffd900" id="messagePassword">Password Moderata</h4>`
+    } else if(countValid === 3){
+        messagePasswordDom.innerHTML=`<h4 class="mt-5" style="color:#fe9a00" id="messagePassword">Password Sicura</h4>`
+    } else if(countValid === 4){
+        messagePasswordDom.innerHTML=`<h4 class="mt-5" style="color:#42a646" id="messagePassword">Password Molto Sicura</h4>`
+    } else{
+         messagePasswordDom.innerHTML=`<h4 class="mt-5" id="messagePassword">Segui le indicazioni Per generare una password sicura</h4>`
+    }
+}
+
+
 // inizializzo le classi per generare il testo delle validazioni
-function generateInnerHtmlValidatorElement(text,flag){
+function generateInnerHtmlValidatorElement(text,flag,icon){
     
-     return `<i class="fa-solid fa-check ${flag}"></i> <span class="${flag}">${text}</span>`
+     return `<i class="fa-solid fa-${icon} ${flag}"></i> <span class="${flag}">${text}</span>`
 } 
 
+
 function MinumCharacters(){
-    if( inputPassword.value.length >= 9 ){  
+    if( inputPassword.value.length >= 9 && inputPassword.value.match(/[a-z]/g)){  
         countValid = countValid + 1 ;
-        characters.innerHTML = generateInnerHtmlValidatorElement('Almeno 9 caratteri','text-success');
+        characters.innerHTML = generateInnerHtmlValidatorElement('Almeno 9 caratteri','text-success','check');
     } else{
-        characters.innerHTML =generateInnerHtmlValidatorElement('Almeno 9 caratteri','text-danger');
+        characters.innerHTML =generateInnerHtmlValidatorElement('Almeno 9 caratteri','text-danger','x');
     } 
 }
 
 function MinumNumber(){
 
     if( inputPassword.value.match(/\w*\d/) ){  
-        number.innerHTML = generateInnerHtmlValidatorElement('Almeno un numero','text-success');
+        number.innerHTML = generateInnerHtmlValidatorElement('Almeno un numero','text-success','check');
         countValid = countValid + 1 ;
     } else{
-        number.innerHTML = generateInnerHtmlValidatorElement('Almeno un numero','text-danger');
+        number.innerHTML = generateInnerHtmlValidatorElement('Almeno un numero','text-danger','x');
 
     }
 }
 
 function MinumUppercase(){
     if (inputPassword.value.match(/[A-Z]/)) {
-        uppercase.innerHTML = generateInnerHtmlValidatorElement('Almeno una maiuscola','text-success');
+        uppercase.innerHTML = generateInnerHtmlValidatorElement('Almeno una maiuscola','text-success','check');
         countValid = countValid + 1 ;
     } else {
-        uppercase.innerHTML = generateInnerHtmlValidatorElement('Almeno una maiuscola','text-danger');
+        uppercase.innerHTML = generateInnerHtmlValidatorElement('Almeno una maiuscola','text-danger','x');
     }
 }
 
 function MinumSpecialCharacters(){
     if (inputPassword.value.match(/[!@#$%^&*(),.?":{}|<>]/)) {
-        charactersSpecial.innerHTML = generateInnerHtmlValidatorElement('Almeno una carattere speciale','text-success');
+        charactersSpecial.innerHTML = generateInnerHtmlValidatorElement('Almeno una carattere speciale','text-success','check');
         countValid = countValid + 1 ;
     } else {
-        charactersSpecial.innerHTML = generateInnerHtmlValidatorElement('Almeno una carattere speciale','text-danger');
+        charactersSpecial.innerHTML = generateInnerHtmlValidatorElement('Almeno una carattere speciale','text-danger','x');
     }
 }
 
@@ -92,6 +109,7 @@ function IsValidInput(){
         MinumNumber();
         MinumSpecialCharacters();
         progressBar();
+        messagePassword()
         console.log(countValid)
     })
 }
